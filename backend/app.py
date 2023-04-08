7c400e6ec0a492c7a00e697761dfc60a625e13e0
Akhil Potla <akhil4400@gmail.com>
2023-04-08 12:44:06 -0600

    Create a user

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_bcrypt import Bcrypt
 from flask_cors import CORS
+from marshmallow import ValidationError
 from pymongo import MongoClient
 
+from schemas import LoginSchema, UserSchema
+
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 client = MongoClient('localhost', 27017)
 db = client.flask_db
@@ -10,10 +15,32 @@ users = db.users
 
 CORS(app)
 
-@app.route('/hello', methods=['GET'])
-def hello_world():
-    users.insert_one({'name': 'Akhil', 'email': 'akhil4400@gmail.com'})
-    return jsonify(message='Hello World!')
+@app.route('/', methods=['POST'])
+def login():
+    data = request.get_json()
+    schema = LoginSchema()
+    try:
+        result = schema.load(data)
+        raise
+        password = data['password']
+        data['password'] = bcrypt.generate_password_hash(password)
+        users.insert_one(data)
+    except ValidationError as err:
+        return jsonify(message=err.messages), 400
+    return jsonify(message="User added!"), 200
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    schema = UserSchema()
+    try:
+        result = schema.load(data)
+        password = data['password']
+        data['password'] = bcrypt.generate_password_hash(password)
+        users.insert_one(data)
+    except ValidationError as err:
+        return jsonify(message=err.messages), 400
+    return jsonify(message="User added!"), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
