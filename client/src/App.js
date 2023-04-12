b53d09706d4dc4ce99e4b1fc1b4f42241e178d03
Akhil Potla <akhil4400@gmail.com>
2023-04-11 19:10:47 -0600

    Using Daybook as the basis for the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -2,28 +2,31 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import axios from 'axios';
 
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
+import Landing from './components/Landing';
 import Login from './components/auth/Login';
-import Profile from './components/Profile';
+import Entry from './components/Entry';
+import Journal from './components/Journal';
 import Register from './components/auth/Register';
 
 
 // Styles
-import './App.css';
+import './css/normalize.css';
+import './css/skeleton.css';
+import './css/custom.css'
+
 
 axios.defaults.baseURL = 'http://127.0.0.1:5000';
 
 const App = () => (
     <Router>
         <Fragment>
-            <Navbar />
             <Route exact path="/" component={Landing}/>
             <section className="container">
                 <Switch>
                     <Route exact path="/register" component={Register}/>
                     <Route exact path="/login" component={Login}/>
-                    <Route exact path="/profile" component={Profile}/>
+                    <Route exact path="/entry" component={Entry}/>
+                    <Route exact path="/journal" component={Journal}/>
                 </Switch>
             </section>
         </Fragment>
