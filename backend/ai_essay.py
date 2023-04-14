aabe8e058c7601dc8453409c42a8e8995446f722
Nelson Morrow <nelsonmorrow@utexas.edu>
2023-04-14 11:22:02 -0500

    AI Essay refactor; better performance

--- backend/ai_essay.py
+++ backend/ai_essay.py
@@ -1,6 +1,7 @@
 import os
 import openai
 import constants
+import utils
 from langchain.document_loaders import TextLoader
 from langchain.document_loaders import PyPDFLoader
 from langchain.agents import load_tools
@@ -23,32 +24,12 @@ import pathlib
 import subprocess
 import tempfile
 from reportlab.pdfgen import canvas
+from langchain.chains import LLMChain
 from langchain.chat_models import ChatOpenAI
 
 os.environ["SERPAPI_API_KEY"] = "ab9f6989f1bca1e2a64b472ac9e999fa8939cd291fa4673bb0a045476fe29277"
 wikipedia = WikipediaAPIWrapper()
 loader = TextLoader("bkk-case-analysis.txt")
-def get_github_docs(repo_owner, repo_name):
-    with tempfile.TemporaryDirectory() as d:
-        subprocess.check_call(
-            f"git clone --depth 1 https://github.com/{repo_owner}/{repo_name}.git .",
-            cwd=d,
-            shell=True,
-        )
-        git_sha = (
-            subprocess.check_output("git rev-parse HEAD", shell=True, cwd=d)
-            .decode("utf-8")
-            .strip()
-        )
-        repo_path = pathlib.Path(d)
-        markdown_files = list(repo_path.glob("*/*.md")) + list(
-            repo_path.glob("*/*.mdx")
-        )
-        for markdown_file in markdown_files:
-            with open(markdown_file, "r") as f:
-                relative_path = markdown_file.relative_to(repo_path)
-                github_url = f"https://github.com/{repo_owner}/{repo_name}/blob/{git_sha}/{relative_path}"
-                yield Document(page_content=f.read(), metadata={"source": github_url})
 
 sources = loader.load()#get_github_docs("yirenlu92", "deno-manual-forked")
 
@@ -59,137 +40,52 @@ for source in sources:
         source_chunks.append(Document(page_content=chunk, metadata=source.metadata))
 search_index = Chroma.from_documents(source_chunks, OpenAIEmbeddings(model="text-embedding-ada-002"))
 
-from langchain.chains import LLMChain
+llm = OpenAI(temperature=0, max_tokens=-1, )
 
 essay_prompt = """This case assignment will explore some of the challenges of taking a new
 product to market. Your role in this case analysis is to develop your own perspective on
 whether you would advocate to bring BKK to market, which of the three options are best,
-and would BKK be successful. Your submitted analysis should detail your position. Write
-a detailed and methodical introductory paragraph for an essay that will include three body
-paragraphs of each at least three sentences. Make sure your introductory paragraph has a
-topic sentence outlining the three paragraphs of your essay. Make your introduction paragraph a minimum of 100 words. Only respond with your introduction paragraph."""
-prompt_template = """ You are a helpful essay writer with great knowledge of the
-pharmaceutical industry and you only are able to write one paragraph responses. Use the context below.
-    Context: {context}
-    Prompt: {topic}
-    Essay: """
+and would BKK be successful. Your submitted analysis should detail your position. """
 
 essay_intro_prompt = PromptTemplate(
-    template=prompt_template, input_variables=["context", "topic"]
+    template=constants.ARGUMENTATIVE_ESSAY_INTRO_PARAGRAPH_YAML, input_variables=[ "prompt", "context"]
 )
 
-llm = OpenAI(temperature=0, max_tokens=-1, )
-
-chain = LLMChain(llm=llm, prompt=essay_intro_prompt)
-
-def gen_introduction(topic):
-    docs = search_index.similarity_search(topic, k=1)
-    print(len(docs))
-    inputs = [{"context": doc.page_content, "topic": topic} for doc in docs]
-    return chain.apply(inputs)
-
-with open("introduction.txt", 'w') as f:
-    introduction = gen_introduction(essay_prompt)[0]["text"]
-    f.write(introduction)
-
-body_paragraph_template = """ You are a highly professional and helpful essay writer with
-great knowledge of the pharmaceutical industry. Argue, in one paragraph, the first idea
-stated in the introduction paragraph given below. Ensure your paragraph is 200 words minimum. Only write One paragraph.
-    Context: {context}
-    Introduction: {introduction}
-    Essay: """
-paragraph_1 = PromptTemplate(
-    template=body_paragraph_template, input_variables=["context", "introduction"]
+body_paragraph = PromptTemplate(
+    template=constants.ARGUMENTATIVE_ESSAY_BODY_PARAGRAPH_YAML, input_variables=["order",       "thesis","context"]
 )
-chain1 = LLMChain(llm=llm, prompt=paragraph_1)
-def generate_body(topic):
-    docs = search_index.similarity_search(topic, k=1)
-    print(len(docs))
-    inputs = [{"context": doc.page_content, "introduction": introduction} for doc in docs]
-    return chain1.apply(inputs)
+conclusion_paragraph = PromptTemplate(template=constants.ARGUMENTATIVE_ESSAY_CONCLUSION_YAML,
+        input_variables=["thesis", "context"])
 
-with open("body_1.txt", 'w') as f:
-    body_1 = generate_body(introduction)[0]["text"]
-    f.write(body_1)
+intro_chain = LLMChain(llm=llm, prompt=essay_intro_prompt)
+body_chain = LLMChain(llm=llm, prompt=body_paragraph)
+conclusion_chain = LLMChain(llm=llm, prompt=conclusion_paragraph)
 
-body_paragraph_template = """ You are a highly professional and helpful essay writer with
-great knowledge of the pharmaceutical industry. Argue, in one paragraph, the second idea
-stated in the introduction paragraph given below. Ensure your paragraph is 200 words
-minimum. Only write ONE paragraph or else you DIE.
-    Context: {context}
-    Introduction: {introduction}
-    Essay: """
-paragraph_2 = PromptTemplate(
-    template=body_paragraph_template, input_variables=["context", "introduction"]
-)
-chain2 = LLMChain(llm=llm, prompt=paragraph_2)
-def generate_body(topic):
-    docs = search_index.similarity_search(topic, k=1)
-    print(len(docs))
-    inputs = [{"context": doc.page_content, "introduction": introduction} for doc in docs]
-    return chain2.apply(inputs)
+def gen_introduction(prompt):
+    docs = search_index.similarity_search(prompt, k=1)
+    inputs = [{"context": doc.page_content, "prompt": prompt} for doc in docs]
+    return intro_chain.apply(inputs)
 
-with open("body_2.txt", 'w') as f:
-    body_2 = generate_body(introduction)[0]["text"]
-    f.write(body_2)
+def gen_body(order, thesis):
+    docs = search_index.similarity_search(thesis, k=1)
+    inputs = [{"order": order, "thesis": thesis, "context": doc.page_content} for doc in docs]
+    return body_chain.apply(inputs)
 
-body_paragraph_template = """ You are a highly professional and helpful essay writer with
-great knowledge of the pharmaceutical industry. Argue, in one paragraph, the third idea
-stated in the introduction paragraph given below. Ensure your paragraph is 200 words
-minimum. Only write ONE paragraph or ELSE YOU WILL DIE.
-    Context: {context}
-    Introduction: {introduction}
-    Essay: """
-paragraph_3 = PromptTemplate(
-    template=body_paragraph_template, input_variables=["context", "introduction"]
-)
-chain3 = LLMChain(llm=llm, prompt=paragraph_3)
-def generate_body(topic):
-    docs = search_index.similarity_search(topic, k=1)
-    print(len(docs))
-    inputs = [{"context": doc.page_content, "introduction": introduction} for doc in docs]
-    return chain3.apply(inputs)
+def gen_conclusion(thesis):
+    docs = search_index.similarity_search(thesis, k=1)
+    inputs = [{"thesis": thesis, "context": doc.page_content} for doc in docs]
+    return conclusion_chain.apply(inputs)
 
-with open("body_3.txt", 'w') as f:
-    body_3 = generate_body(introduction)[0]["text"]
-    f.write(body_3)
-
-conclusion_template = """ You are a highly professional and helpful essay writer with
-great knowledge of the pharmaceutical industry. Given the introduction paragraph below,
-Remember to make a recommendation. write the conclusion paragraph. Ensure your paragraph
-is 200 words minimum. Only write One paragraph.
-    Context: {context}
-    Introduction: {introduction}
-    Essay: """
-paragraph_2 = PromptTemplate(
-    template=body_paragraph_template, input_variables=["context", "introduction"]
-)
-chain = LLMChain(llm=llm, prompt=paragraph_2)
-def generate_body(topic):
-    docs = search_index.similarity_search(topic, k=1)
-    print(len(docs))
-    inputs = [{"context": doc.page_content, "introduction": introduction} for doc in docs]
-    return chain.apply(inputs)
-
-with open("conclusion.txt", 'w') as f:
-    conclusion = generate_body(introduction)[0]["text"]
-    f.write(conclusion)
-
+introduction = gen_introduction(essay_prompt)[0]["text"]
+thesis = utils.promptGPT(constants.EXTRACT_TOPIC_SENTENCE, introduction)
+print(thesis)
 with open("essay.txt", 'w') as f:
     f.write(introduction)
     f.write("\n")
-    f.write(body_1)
+    f.write(gen_body("1", thesis)[0]["text"])
     f.write("\n")
-    f.write(body_2)
+    f.write(gen_body("2", thesis)[0]["text"])
     f.write("\n")
-    f.write(body_3)
+    f.write(gen_body("3", thesis)[0]["text"])
     f.write("\n")
-    f.write(conclusion)
-
-essay = introduction + "\n" + body_1 + "\n" + body_2 + "\n" + body_3 + "\n" + conclusion
-llm = OpenAI(model_name="gpt-4", max_tokens=-1)
-intro_gpt = llm("""Rewrite this essay to be more professional and compelling. You are an
-expert consultant and are making your arguments more clear. Don't just use big words, stress
-logical coherence and creativity. Minimum 800 words. : {}""""".format(essay))
-with open("essay-200-iq.txt", 'w') as f:
-    f.write(intro_gpt)
+    f.write(gen_conclusion(thesis)[0]["text"])
