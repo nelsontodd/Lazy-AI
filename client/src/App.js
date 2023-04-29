b9b7bd0b6c6f0a3e65de186c65d85fdc5888d9c6
Akhil Potla <akhil4400@gmail.com>
2023-04-29 09:56:21 -0600

    Increase margin on top

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import axios from 'axios';
 
+import Box from '@mui/material/Box';
+
 import Landing from './components/Landing';
 import Login from './components/auth/Login';
 import Homework from './components/Homework';
@@ -11,18 +13,20 @@ import SignUp from './components/auth/SignUp';
 axios.defaults.baseURL = 'http://127.0.0.1:5000';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Route exact path="/" component={Landing}/>
-      <section className="container">
-        <Switch>
-          <Route exact path="/signup" component={SignUp}/>
-          <Route exact path="/login" component={Login}/>
-          <Route exact path="/homework" component={Homework}/>
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Box mt={5}>
+    <Router>
+      <Fragment>
+        <Route exact path="/" component={Landing}/>
+        <section className="container">
+          <Switch>
+            <Route exact path="/signup" component={SignUp}/>
+            <Route exact path="/login" component={Login}/>
+            <Route exact path="/homework" component={Homework}/>
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Box>
 );
 
 export default App;
