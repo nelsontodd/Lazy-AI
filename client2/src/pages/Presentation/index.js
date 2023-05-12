76d2def4724eda4b922d60a31db5070800ae96f2
Akhil Potla <akhil4400@gmail.com>
2023-05-12 13:24:45 -0600

    Fancy button is now for sign in

--- client2/src/pages/Presentation/index.js
+++ client2/src/pages/Presentation/index.js
@@ -52,9 +52,9 @@ function Presentation() {
       <DefaultNavbar
         routes={routes}
         action={{
-          type: "external",
-          route: "https://www.creative-tim.com/product/material-kit-react",
-          label: "free download",
+          type: "internal",
+          route: "/sign-in",
+          label: "sign in",
           color: "info",
         }}
         sticky
