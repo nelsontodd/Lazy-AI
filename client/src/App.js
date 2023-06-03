cb13f6615a6759fbea75a8739948fd7168fafacd
Akhil Potla <akhil4400@gmail.com>
2023-06-03 10:47:06 -0600

    Sign up page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
 import './App.css';
 
 import Landing from './components/Landing';
 import Navigation from './components/Navigation';
+import SignUp from './components/SignUp';
+
+
+axios.defaults.baseURL = 'http://127.0.0.1:5000';
 
 function App() {
   return (
@@ -12,6 +17,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </Router>
     </div>
--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import axios from 'axios';
 // import reportWebVitals from './reportWebVitals';
 
 import './App.scss';
+
+axios.defaults.baseURL = 'http://127.0.0.1:5000';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
