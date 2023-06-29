33877c2be9607aa6d1b9142c5fe4271ea1fae02a
Akhil Potla <akhil4400@gmail.com>
2023-06-29 18:12:38 -0500

    Homework table with button to create new assignment

--- client/src/components/Homework/index.js
+++ client/src/components/Homework/index.js
@@ -1,13 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import NewHomework from './NewHomework';
+import Table from 'react-bootstrap/Table';
 
-import { isLoggedIn } from '../../helpers/setAuthToken';
+
+
+import { getCookies, isLoggedIn } from '../../helpers/setAuthToken';
 
 const Homework = () => {
   let navigate = useNavigate();
+  const [assignments, setAssignments] = useState([]);
 
   useEffect(() => {
     if (!isLoggedIn()) {
@@ -15,9 +20,51 @@ const Homework = () => {
     }
   });
 
+  useEffect(() => {
+    getAssignments()
+  }, []);
+
+  const getAssignments = async () => {
+    const token = getCookies().token;
+    const headers = {
+      headers: {
+        'x-auth-token': token
+      },
+    };
+    const res = await axios.get('/homework', headers);
+    console.log(JSON.stringify(res.data));
+    setAssignments(res.data.assignments);
+  }
+
+  const renderAssignments = () => {
+    if (assignments == null) {
+      return null;
+    } else {
+      return (
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Assignment Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {assignments.map((assignment, index) => (
+              <tr key={index}>
+                <td>{assignment.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )
+    }
+  }
+
   return (
     <Container className="mt-5">
-      <NewHomework />
+      <Button href="/homework/new" className="mb-3 text-white">
+        New Assignment
+      </Button>
+      { renderAssignments() }
     </Container>
   );
 }
