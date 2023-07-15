7d354dc77e5ab3176fd85740bea06f6f1ee7a727
Akhil Potla <akhil4400@gmail.com>
2023-07-15 16:54:11 -0600

    Backend stuff for square payments

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,13 @@
+import os
+
 from bson.objectid import ObjectId
 from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS, cross_origin
 from jwt import ExpiredSignatureError
 from marshmallow import ValidationError
 from pymongo import MongoClient
+from square.client import Client
+from uuid import uuid4
 
 from authentication import create_token, get_user
 from schemas import FileSchema, LoginSchema, UserSchema
@@ -16,6 +20,12 @@ import lazy_ai
 app = Flask(__name__)
 CORS(app)
 
+client = Client(
+    access_token=os.environ['SQUARE_ACCESS_TOKEN'],
+    environment='sandbox'
+)
+
+
 @app.route('/homework', methods=['POST'])
 def create_solution():
     #TODO: Get username, prompt for user full name, assignment description, title (optional)
@@ -25,12 +35,12 @@ def create_solution():
         if file is not None:
             result = FileSchema().load(file)
             hwsolve = lazy_ai.LazyAI(
-                file.filename, "{} solutions".format(file.filename),
-                "Speech Language Pathology Exam Study Guide",
-                "nelsontodd",
-                "Nelson Morrow",
-                "Homework 4"
-            )
+                    file.filename, "{} solutions".format(file.filename),
+                    "Speech Language Pathology Exam Study Guide",
+                    "nelsontodd",
+                    "Nelson Morrow",
+                    "Homework 4"
+                    )
             file.seek(0)
             file.save(hwsolve.input_rel_path(file.filename))
             solutions = '{}.pdf'.format(hwsolve.solutions_pdf())
@@ -41,6 +51,28 @@ def create_solution():
     except ValidationError as err:
         return jsonify(message=err.messages), 400
     return jsonify(message="Server error"), 500
+
+@app.route('/payment', methods=['POST'])
+def process_payment():
+    data = request.get_json()
+    if 'sourceId' in data:
+        token = data['sourceId']
+        create_payment_response = client.payments.create_payment(
+            body={
+                'source_id': token,
+                'idempotency_key': str(uuid4()),
+                'amount_money': {
+                    'amount': 100,  # $1.00 charge
+                    'currency': 'USD',
+                },
+            }
+        )
+        return jsonify(message='Payment route'), 200
+    else:
+        return jsonify(
+                message='Please enter valid payment information'
+                ), 500
+
 
 
 if __name__ == '__main__':
