d2feda473eb8ea296167122efed7f4c693f06f74
Akhil Potla <akhil4400@gmail.com>
2023-07-17 20:48:18 -0600

    Remove unused import

--- client/src/components/Landing/Navigation.js
+++ client/src/components/Landing/Navigation.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 import logo from '../../logoipsum-298.svg';
