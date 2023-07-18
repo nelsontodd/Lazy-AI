7695397904c244d3e2b968c37ef3dc71da5e5b03
Akhil Potla <akhil4400@gmail.com>
2023-07-18 07:53:03 -0600

    Adding additional field necessary for processing

--- client/src/components/Homework/FileUploader.jsx
+++ client/src/components/Homework/FileUploader.jsx
@@ -9,6 +9,9 @@ import RenderPDF from './RenderPDF';
 
 const FileUploader = () => {
   const [file, setFile] = useState(null);
+  const [hasLatex, setHasLatex] = useState(false);
+  const [isHomework, setIsHomework] = useState(true);
+  const [name, setName] = useState(null);
   const [processingSolution, setProcessingSolution] = useState(false);
 
   const onFileChange = (e) => {
@@ -20,12 +23,23 @@ const FileUploader = () => {
     }
   }
 
+  const handleHomeworkRadioButton = (e) => {
+    setIsHomework(!isHomework);
+  }
+
+  const handleLatexRadioButton = (e) => {
+    setHasLatex(!hasLatex);
+  }
+
   const onSubmit = async (token, verifiedBuyer) => {
     if (file) {
       const formData = new FormData();
       try {
         formData.append('file', file);
         formData.append('fileName', file.name);
+        formData.append('hasLatex', hasLatex);
+        formData.append('isHomework', isHomework);
+        formData.append('name', name);
         formData.append('sourceId', token.token);
         setProcessingSolution(true);
         const headers = {
@@ -37,7 +51,6 @@ const FileUploader = () => {
         const res = await axios.post('/homework', formData, headers);
         const blob = new Blob([res.data], { type: 'application/pdf' });
         saveAs(blob, 'solutions.pdf');
-        setProcessingSolution(false);
       } catch (err) {
         const errorMessage = err.response.data.message;
         alert(errorMessage);
@@ -45,6 +58,7 @@ const FileUploader = () => {
     } else {
       alert('No file selected.');
     }
+    setProcessingSolution(false);
   }
 
   const headerText = () => {
@@ -69,9 +83,59 @@ const FileUploader = () => {
                   id="fileUploader"
                   onChange={(e) => onFileChange(e)}
                 />
+                <p>Select a file smaller than 5MB before uploading</p>
+                <p className="mt-3">
+                  <b>Full Name</b>
+                </p>
+                <Form.Control
+                  type="text"
+                  id="name"
+                  value={name}
+                  placeholder="Your Full Name"
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+                <p className="mt-3">
+                  <b>Does your assignment have equations? (Example: Latex, physics,
+                    chemistry, etc)</b>
+                </p>
+                <Form.Check
+                  value={true}
+                  type="radio"
+                  aria-label="radio 1"
+                  label="Yes"
+                  onChange={handleLatexRadioButton}
+                  checked={hasLatex === true}
+                />
+                <Form.Check
+                  value={false}
+                  type="radio"
+                  aria-label="radio 2"
+                  label="No"
+                  onChange={handleLatexRadioButton}
+                  checked={hasLatex === false}
+                />
+                <p className="mt-3">
+                  <b>Is this a homework assignment or a study guide?</b>
+                </p>
+                <Form.Check
+                  value={true}
+                  type="radio"
+                  aria-label="radio 1"
+                  label="Homework Assignment"
+                  onChange={handleHomeworkRadioButton}
+                  checked={isHomework === true}
+                />
+                <Form.Check
+                  value={false}
+                  type="radio"
+                  aria-label="radio 2"
+                  label="Study Guide"
+                  onChange={handleHomeworkRadioButton}
+                  checked={isHomework === false}
+                />
               </Form.Group>
-              <p>Select a file smaller than 5MB before uploading</p>
-              <h6>Get solutions for $1</h6>
+              <h6 className="mt-3">Get solutions for $1</h6>
               <PaymentForm
                 applicationId="sandbox-sq0idb-VjaXQsDt014XTRq4IY14aw"
                 cardTokenizeResponseReceived={
